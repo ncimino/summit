4a22e9d369a2d4f6383a40b064186cd42563afb4
summit <summit@econtriver.com>
2012-10-28 17:35:15 -0600

    added a mechanism to clear fields

--- app/assets/javascripts/websites.js
+++ app/assets/javascripts/websites.js
@@ -6,12 +6,12 @@ function auto_fill(id, value, force) {
 
 function set_defaults(name, force) {
     auto_fill("#website_domain", name + ".econtriver.com", force)
-    auto_fill("#website_deploy_path","/srv/www/" + name + ".econtriver.com/", force)
+    auto_fill("#website_deploy_path","/srv/www/" + name + ".econtriver.com", force)
     auto_fill("#website_post_receive_path","/home/g/repositories/" + name + ".git/hooks/post-receive", force)
     auto_fill("#website_nginx_path","/etc/nginx/sites-available/" + name + ".econtriver.com", force)
     auto_fill("#website_enabled_nginx_path","/etc/nginx/sites-enabled/" + name + ".econtriver.com", force)
-    auto_fill("#website_git_repo_path","/home/g/repositories/" + name + ".git/", force)
-    auto_fill("#website_enabled_git_path","/srv/www/git.econtriver.com/public_html/" + name + ".git/", force)
+    auto_fill("#website_git_repo_path","/home/g/repositories/" + name + ".git", force)
+    auto_fill("#website_enabled_git_path","/srv/www/git.econtriver.com/public_html/" + name + ".git", force)
 }
 
 $(document).ready(function(){
@@ -20,5 +20,9 @@ $(document).ready(function(){
     });
     $("#reload").click(function() {
         set_defaults($("#website_name").val().toLowerCase(), true);
+    });
+    $(".clearfield").click(function() {
+        $(this).prev("input.text").val('');
+        $(this).prev("div").children("input.text").val('');
     });
 });
