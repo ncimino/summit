0b24e6b4a0224f5002cf222c9c1f8e9b5ba4f1ce
summit <summit@econtriver.com>
2012-10-29 00:40:03 -0600

    move the gitolite pull to common tmp

--- config/initializers/summit.rb
+++ config/initializers/summit.rb
@@ -3,7 +3,7 @@ Summit::Application.configure do
   # git_deploy_loc is the location of the git server and user used for deployment
   config.git_deploy_loc = "g@econtriver.com"
   # gitolite_tmp is where the gitolite repo is checked out to for adding users
-  config.gitolite_tmp = Rails.root.join('tmp', 'gitolite-admin')
+  config.gitolite_tmp = File.join('/tmp', 'gitolite-admin')
   # gitolite_user is the default user that will be added to the repo for deployment
   config.gitolite_user = '@nik'
   # valid_deploy_path is used to validate deploy paths, set to ,* to remove requirement
